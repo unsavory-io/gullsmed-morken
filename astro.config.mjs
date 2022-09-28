3e19c6b1fdac77b812ea546acadb4cbf63ce892c
Alexander Prestmo <aprestmo@gmail.com>
2022-09-28 23:30:13 +0200

    Styles for pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,9 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://gullsmed-morken.netlify.app',
   integrations: [mdx(), sitemap()],
-  vite: {
-    css: {
-      preprocessorOptions: {
-        scss: {
-          // additionalData: `@import "src/styles/scss/base.scss";`,
-        },
-      },
-    },
-  },
 });
